c4093e490a9620f0b8c08401ec0cf0936760c657
Evgenii Koniakhin <evgenii.koniakhin@t-systems.ru>
2019-02-17 16:20:58 +0300

    VR-0: init repo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <div class="container">
+        <div class="inner">
+          <div class="logo"/>
+          <nav class="main-nav">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <main class="content">
+      <div class="container">
+        <h1 class="title is-1">Vue-router</h1>
+        <p>
+          Creating a Single-page Application with Vue + Vue Router is dead simple. With Vue.js, we are already composing our application with components. When adding Vue Router to the mix, all we need to do is map our components to the routes and let Vue Router know where to render them.
+        </p>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import './styles/styles.scss';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
