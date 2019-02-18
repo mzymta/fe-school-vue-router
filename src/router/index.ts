b18d976cfdbdef44a4f601b8e86435f46ff6c506
Evgenii Koniakhin <evgenii.koniakhin@t-systems.ru>
2019-02-18 14:28:28 +0300

    VR-2: router construction options

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,10 @@ import About from '../views/About.vue';
 Vue.use(Router);
 
 export default new Router({
+  base: '/app',
+  mode: 'history',
+  linkActiveClass: 'is-active',
+  linkExactActiveClass: 'is-exact-active',
   routes: [
     {
       path: '/',
@@ -15,5 +19,11 @@ export default new Router({
       path: '/about',
       component: About
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { x: 0, y: 0 };
+  }
 });
